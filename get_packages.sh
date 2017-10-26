3dab6d04ead76dcf5ecbc0e7e67a715dd8e1f9f5
Shawon Ashraf <ShawonAshraf@users.noreply.github.com>
2017-10-27 00:17:47 +0600

    INIT

---
+++ get_packages.sh
@@ -0,0 +1,4 @@
+# gets the installed package names
+# and lists them in the file package_list.txt
+
+apm list --installed --bare > package_list.txt
---
+++ install_package.sh
@@ -0,0 +1,3 @@
+# installs packages listed in the file
+
+apm install --packages-file package_list.txt
